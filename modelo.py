1f3e705d26db8037c31ad417458cc3a64f5bf108
lucas997 <lucasgojer@gmail.com>
2023-02-23 20:22:52 -0300

    adicionando os métodos de herança e polimorfismo ao arquivo python

--- modelo.py
+++ modelo.py
@@ -1,20 +1,55 @@
-class Filme:
-    def __init__(self, nome, ano, duracao):
-       self.nome = nome
+class Programa:
+    def __init__(self, nome, ano):
+       self._nome = nome.title()
        self.ano = ano
-       self.duracao = duracao
-    
+       self._likes = 0
 
-class Serie:
+    @property
+    def likes(self):
+        return self._likes
+    
+    def dar_like(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+    
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+    def __str__(self):
+        return f'{self._nome} - : {self.ano} - {self._likes} Likes'
+
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+       super().__init__(nome, ano)
+       self.duracao = duracao
+
+    def __str__(self):
+        return f'{self._nome} - : {self.ano} - {self.duracao} - min {self._likes} Likes'
+
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-       self.nome = nome
-       self.ano = ano
+       super().__init__(nome, ano)
        self.temporadas = temporadas
+       
+    def __str__(self):
+        return f'{self._nome} - : {self.ano} - {self.temporadas} - temporadas {self._likes} Likes'
     
     
 vingadores = Filme('Vingadores - guerra infinita', 2018, 160)
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} ' f'- Duração: {vingadores.duracao}')
+vingadores.dar_like()
 
-atlanta = Serie('Atlanta', 2018, 2)
 
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} ' f'- Temporadas: {atlanta.temporadas}')
+
+atlanta = Serie('atlanta', 2018, 2)
+atlanta.dar_like()
+atlanta.dar_like()
+
+
+filmes_e_series = [vingadores, atlanta]
+
+for programa in filmes_e_series:
+    print(programa)
