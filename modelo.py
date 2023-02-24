1eb049cbc43e4cdbd666349464737e7386e49246
lucas997 <lucasgojer@gmail.com>
2023-02-24 17:28:36 -0300

    updates nas classes e incrementando os conceitos de herança e Duck Typing

--- modelo.py
+++ modelo.py
@@ -39,17 +39,21 @@ class Serie(Programa):
         return f'{self._nome} - : {self.ano} - {self.temporadas} - temporadas {self._likes} Likes'
 
 
+
+
 class Playlist:
     def __init__(self, nome, programas):
         self.nome = nome
         self._programas = programas
+
+    def __getitem__(self, item):
+        return self._programas[item]
         
     @property
     def listagem(self):
         return self._programas
     
-    @property
-    def tamanho(self):
+    def __len__(self):
         return len(self._programas)
     
     
@@ -74,10 +78,10 @@ atlanta.dar_like()
 filmes_e_series = [vingadores, atlanta, demolidor, tmep]
 playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
-print(f'Tamanho da playlist: {len(playlist_fim_de_semana.listagem)}')
+print(f'Tamanho da playlist: {len(playlist_fim_de_semana)}')
 
 
 
-for programa in playlist_fim_de_semana.listagem:
+for programa in playlist_fim_de_semana:
     print(programa)
     
